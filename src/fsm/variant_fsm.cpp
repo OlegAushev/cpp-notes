5182b13850671ede898b89b3c4fb37e7c6783d59
Oleg Aushev <aushevom@gmail.com>
2024-05-28 01:40:38 +0300

    add variant fsm prototype

---
+++ src/fsm/variant_fsm.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <variant>
+
+
+template<typename FsmType, typename... States>
+class fsm {
+private:
+    using state_variant_t = std::variant<States...>;
+    state_variant_t _state;
+public:
+    template<typename Event>
+    void dispatch(Event&& event) {
+        auto& child = static_cast<FsmType&>(*this);
+        const auto& visitor = [&](auto& state) -> std::optional<state_variant_t> {
+            return child.on_event(state, std::forward(event));
+        };
+        const auto& next_state = std::visit(visitor, _state);
+        if (next_state.has_value()) {
+            _state = next_state.value();
+        }
+    }
+
+
+};
+
+
+int main() {
+
+    return 0;
+}
